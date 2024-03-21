990eebd72e5aeb9d9f8b4eedb626052dfbd9c4ae
Brennan <113397554+abikuneebus@users.noreply.github.com>
2024-03-21 15:51:27 -0400

    Style: Add TSX doc & stylesheet for swarm effect

---
+++ src/components/Swarm.tsx
@@ -0,0 +1,11 @@
+// import React, { useEffect } from 'react';
+// import styles from './Swarm.module.scss';
+//
+// interface Shape {
+//   id: number;
+//   x: number;
+//   y: number;
+//   vx: number;
+//   vy: number;
+//   size: number;
+// }
